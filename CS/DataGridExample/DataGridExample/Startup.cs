868a426558ca8aebc9c10464a1309442f825af26
Elena Dvoryadkina <elena.dvoryadkina@devexpress.com>
2021-07-01 14:10:01 +0300

    Added an example with DataGridView

---
+++ CS/DataGridExample/DataGridExample/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+using DevExpress.Maui.DataGrid;
+
+namespace DataGridExample
+{
+	public class Startup : IStartup
+	{
+		public void Configure(IAppHostBuilder appBuilder)
+		{
+			appBuilder
+				.ConfigureMauiHandlers((_, handlers) => handlers.AddHandler<DataGridView, DataGridViewHandler>())
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+		}
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# DevExpress Data Grid for .NET MAUI
+
+Open the solution in Visual Studio 16.11 Preview 2 and restore NuGet packages to run the application:
+
+1. [Obtain your NuGet feed URL](http://docs.devexpress.com/GeneralInformation/116042/installation/install-devexpress-controls-using-nuget-packages/obtain-your-nuget-feed-url).
+2. Register the DevExpress NuGet feed as a package source.
+3. Restore all NuGet packages for the solution.
+
+<img src="./img/devexpress-maui-data-grid.png"/>
