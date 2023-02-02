24ec4065b2fdbb497231872810245d67a2aa9d42
sameishchev.anton <anton.sameishchev@devexpress.com>
2023-02-02 15:22:07 +0400

    solution file updated

---
+++ CS/DataGridExample/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using System.Globalization;
+
+namespace DataGridExample
+{
+    public class ValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is null)
+                return null;
+            bool isenable = true;
+
+            string test = value.ToString();
+            if (string.IsNullOrEmpty(value.ToString()))
+            {
+                isenable = false;
+            }
+            return isenable;
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
